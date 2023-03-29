3d4f868c36be8500a9f47426092805436142a558
ajaykumar1609 <kotaajaykumar@iitbhilai.ac.in>
2023-03-29 20:42:46 +0530

    added database

--- app.py
+++ app.py
@@ -2,40 +2,38 @@ from flask import Flask, request
 import openai
 from twilio.twiml.messaging_response import MessagingResponse
 import os
+import mysql.connector
 
 # Init the Flask App
 app = Flask(__name__)
 
 # Initialize the OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-openai.api_key = "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"
+# Initialize the MySQL database connection details
+MYSQLHOST = os.getenv("MYSQLHOST")
+MYSQLPORT = os.getenv("MYSQLPORT")
+MYSQLUSER = os.getenv("MYSQLUSER")
+MYSQLPASSWORD = os.getenv("MYSQLPASSWORD")
+MYSQLDATABASE = os.getenv("MYSQLDATABASE")
 
-def generate_answer(question):
-    model_engine = "davinci"
-    prompt = (f"Q: {question}\n"
-              "A:")
-
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    answer = response.choices[0].text.strip()
-    for i in range(len(answer)):
-        if answer[i]=="Q":
-            if answer[i+1]==":":
-                return answer[:i]
-
-    return answer
-
+# Define a function to connect to the MySQL database
+def connect_db():
+    try:
+        connection = mysql.connector.connect(
+            host=MYSQLHOST,
+            port=MYSQLPORT,
+            user=MYSQLUSER,
+            password=MYSQLPASSWORD,
+            database=MYSQLDATABASE
+        )
+        return connection
+    except Exception as e:
+        print("Error connecting to the database:", e)
 
 # Call OpenAI API to generate response
-def generate_response(question):
-    prompt = question.strip()
+def generate_response(question, history):
+    prompt = f"{history} Q: {question}\nA:"
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt,
@@ -49,21 +47,44 @@ def generate_response(question):
     response_text = response.choices[0].text.strip()
     return response_text
 
-
 # Define a route to handle incoming requests
 @app.route('/whatgpt', methods=['POST'])
 def whatgpt():
-    print("bot is running")
+    print("Bot is running")
     incoming_que = request.values.get('Body', '').lower()
     print("Question: ", incoming_que)
-    # Generate the answer using GPT-3
-    answer = generate_answer(incoming_que)
-    print("BOT Answer: ", answer)
+
+    # Connect to the MySQL database
+    connection = connect_db()
+
+    # Get the conversation history from the database
+    cursor = connection.cursor()
+    cursor.execute("SELECT history FROM conversation_history ORDER BY id DESC LIMIT 1")
+    result = cursor.fetchone()
+    if result is None:
+        history = ""
+    else:
+        history = result[0]
+
+    # Generate the response using GPT-3
+    answer = generate_response(incoming_que, history)
+
+    # Store the conversation history in the database
+    history += f"Q: {incoming_que}\nA: {answer}\n"
+    cursor.execute("INSERT INTO conversation_history (history) VALUES (%s)", (history,))
+    connection.commit()
+
+    # Close the database connection
+    cursor.close()
+    connection.close()
+
+    print("Bot Answer: ", answer)
+
+    # Send the response to Twilio
     bot_resp = MessagingResponse()
     msg = bot_resp.message()
     msg.body(answer)
     return str(bot_resp)
-
 
 # Run the Flask app
 if __name__ == '__main__':
