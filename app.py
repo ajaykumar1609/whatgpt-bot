e3e9d3234f85fa19f4e0f68d614584c9ecde9b33
ajaykumar1609 <kotaajaykumar@iitbhilai.ac.in>
2023-03-30 11:33:33 +0530

    added

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def generate_response(prompt, history=""):
 
     # Generate response using GPT-3
     response = openai.Completion.create(
-        engine="davinci-text-003",
+        engine="davinci",
         prompt=prompt,
         max_tokens=100,
         n=1,
